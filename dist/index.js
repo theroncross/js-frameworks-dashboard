cc93d0ff26948499b80d567b0cc23b93f86cd430
theroncross <theroncross@gmail.com>
2016-09-06 19:22:47 -0500

    Add select menu

--- dist/index.js
+++ dist/index.js
@@ -1,65 +1,86 @@
 'use strict';
 
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
+
 var app = document.getElementById('app');
 var rootUrl = 'https://api.github.com/';
 var NS = 'http://www.w3.org/2000/svg';
+
 var setAttributes = function setAttributes(el, attrs) {
   for (var attr in attrs) {
     el.setAttributeNS(null, attr, attrs[attr]);
   }
 };
+
 var SVGElement = function SVGElement(type) {
   var svg = document.createElementNS(NS, type);
   return svg;
 };
 
-var newGraph = function newGraph(params) {
-  var route = params.route;
-  var id = params.id;
-  var color = params.color;
-  var _params$width = params.width;
-  var width = _params$width === undefined ? 1030 : _params$width;
-  var _params$height = params.height;
-  var height = _params$height === undefined ? 140 : _params$height;
+var newGraph = function newGraph(attrs) {
+  var data = attrs.data;
+  var id = attrs.id;
+  var color = attrs.color;
+  var _attrs$width = attrs.width;
+  var width = _attrs$width === undefined ? 1030 : _attrs$width;
+  var _attrs$height = attrs.height;
+  var height = _attrs$height === undefined ? 140 : _attrs$height;
+
+  var barWidth = width / data.all.length;
+  var bar = void 0,
+      rect = void 0;
+
+  var graph = SVGElement("svg");
+  setAttributes(graph, { height: height, width: width, id: id });
+
+  var avg = data.all.reduce(function (w, a) {
+    return w + a;
+  }) / data.all.length;
+  var avgLine = SVGElement("line");
+  setAttributes(avgLine, {
+    "x1": "0",
+    "x2": width,
+    "y1": '' + (100 - avg),
+    "y2": '' + (100 - avg),
+    "stroke-width": "1px",
+    "stroke": "orange"
+  });
+
+  data.all.forEach(function (week, i) {
+    bar = SVGElement("g");
+    setAttributes(bar, {
+      "transform": 'translate(' + barWidth * i + ', 0)'
+    });
+
+    rect = SVGElement("rect");
+    setAttributes(rect, {
+      "width": '' + (barWidth - 2),
+      "height": week,
+      "y": '' + (100 - week),
+      "fill": color
+    });
+
+    bar.appendChild(rect);
+    graph.appendChild(bar);
+  });
+
+  graph.appendChild(avgLine);
+  app.appendChild(graph);
+};
+
+var fetchData = function fetchData(_ref) {
+  var route = _ref.route;
+
+  var attrs = _objectWithoutProperties(_ref, ['route']);
 
   fetch('' + rootUrl + route).then(function (response) {
     return response.json();
   }).then(function (data) {
-    var bar = void 0,
-        rect = void 0;
-    var barWidth = width / data.all.length;
-    var graph = SVGElement("svg");
-    setAttributes(graph, { height: height, width: width, id: id });
-
-    var avg = data.all.reduce(function (w, a) {
-      return w + a;
-    }) / data.all.length;
-    var avgLine = SVGElement("line");
-    setAttributes(avgLine, {
-      "x1": "0",
-      "x2": width,
-      "y1": '' + (100 - avg),
-      "y2": '' + (100 - avg),
-      "stroke-width": "1px",
-      "stroke": "orange"
-    });
-    data.all.forEach(function (week, i) {
-      bar = SVGElement("g");
-      setAttributes(bar, {
-        "transform": 'translate(' + barWidth * i + ', 0)'
-      });
-      rect = SVGElement("rect");
-      setAttributes(rect, {
-        "width": '' + (barWidth - 2),
-        "height": week,
-        "y": '' + (100 - week),
-        "fill": color
-      });
-      bar.appendChild(rect);
-      graph.appendChild(bar);
-    });
-    graph.appendChild(avgLine);
-    app.appendChild(graph);
+    newGraph(_extends({}, attrs, { data: data }));
+  }).catch(function (error) {
+    console.log('Error loading data: ' + error.mesage);
   });
 };
 
@@ -81,31 +102,47 @@ var frameworks = [{
   color: "purple"
 }];
 
-var _iteratorNormalCompletion = true;
-var _didIteratorError = false;
-var _iteratorError = undefined;
+var renderGraphs = function renderGraphs(partialRoute) {
+  var _iteratorNormalCompletion = true;
+  var _didIteratorError = false;
+  var _iteratorError = undefined;
 
-try {
-  for (var _iterator = frameworks[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-    var framework = _step.value;
-
-    newGraph({
-      route: 'repos/' + framework.org + '/' + framework.repo + '/stats/participation',
-      id: framework.repo,
-      color: framework.color
-    });
-  }
-} catch (err) {
-  _didIteratorError = true;
-  _iteratorError = err;
-} finally {
   try {
-    if (!_iteratorNormalCompletion && _iterator.return) {
-      _iterator.return();
+    for (var _iterator = frameworks[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+      var framework = _step.value;
+      var org = framework.org;
+      var repo = framework.repo;
+      var color = framework.color;
+
+      fetchData({
+        route: 'repos/' + org + '/' + repo + '/' + partialRoute,
+        id: repo,
+        color: color
+      });
     }
+  } catch (err) {
+    _didIteratorError = true;
+    _iteratorError = err;
   } finally {
-    if (_didIteratorError) {
-      throw _iteratorError;
+    try {
+      if (!_iteratorNormalCompletion && _iterator.return) {
+        _iterator.return();
+      }
+    } finally {
+      if (_didIteratorError) {
+        throw _iteratorError;
+      }
     }
   }
-}
+};
+
+window.onload = function () {
+  var form = document.getElementById("stat-selector");
+  form.onchange = function (event) {
+    if (event.target.value === "participation") {
+      renderGraphs('stats/participation');
+    } else {
+      renderGraphs('issues/comments');
+    }
+  };
+};
--- src/index.js
+++ src/index.js
@@ -1,57 +1,69 @@
 const app = document.getElementById('app');
 const rootUrl = 'https://api.github.com/'
 const NS='http://www.w3.org/2000/svg';
+
 const setAttributes = (el, attrs) => {
   for(let attr in attrs) {
     el.setAttributeNS(null, attr, attrs[attr]);
   }
 };
+
 const SVGElement = (type) => {
   const svg=document.createElementNS(NS, type);
   return svg;
 };
 
-const newGraph = (params) => {
-  const { route, id, color, width = 1030, height = 140 } = params;
+const newGraph = (attrs) => {
+  const { data, id, color, width = 1030, height = 140 } = attrs;
+  const barWidth = width / data.all.length;
+  let bar, rect;
+
+  const graph = SVGElement("svg");
+  setAttributes(graph, { height, width, id });
+
+  const avg = data.all.reduce((w, a) => { return w + a }) / data.all.length;
+  const avgLine = SVGElement("line");
+  setAttributes(avgLine, {
+    "x1": "0",
+    "x2": width,
+    "y1": `${100 - avg}`,
+    "y2": `${100 - avg}`,
+    "stroke-width": "1px",
+    "stroke": "orange"
+  });
+
+  data.all.forEach((week, i) => {
+    bar = SVGElement("g");
+    setAttributes(bar, {
+      "transform": `translate(${barWidth * i}, 0)`
+    });
+
+    rect = SVGElement("rect");
+    setAttributes(rect, {
+      "width": `${barWidth - 2}`,
+      "height": week,
+      "y": `${100 - week}`,
+      "fill": color
+    });
+
+    bar.appendChild(rect);
+    graph.appendChild(bar);
+  });
+
+  graph.appendChild(avgLine);
+  app.appendChild(graph);
+};
+
+const fetchData = ({ route, ...attrs }) => {
   fetch(`${rootUrl}${route}`)
   .then((response) => {
     return response.json()
   })
-  .then((data) => {
-    let bar, rect;
-    const barWidth = width / data.all.length;
-    const graph = SVGElement("svg");
-    setAttributes(graph, { height, width, id });
-
-    const avg = data.all.reduce((w, a) => { return w + a }) / data.all.length;
-    const avgLine = SVGElement("line");
-    setAttributes(avgLine, {
-      "x1": "0",
-      "x2": width,
-      "y1": `${100 - avg}`,
-      "y2": `${100 - avg}`,
-      "stroke-width": "1px",
-      "stroke": "orange"
-    });
-    data.all.forEach((week, i) => {
-      bar = SVGElement("g");
-      setAttributes(bar, {
-        "transform": `translate(${barWidth * i}, 0)`
-      });
-      rect = SVGElement("rect");
-      setAttributes(rect, {
-        "width": `${barWidth - 2}`,
-        "height": week,
-        "y": `${100 - week}`,
-        "fill": color
-      });
-      bar.appendChild(rect);
-      graph.appendChild(bar);
-    });
-    graph.appendChild(avgLine);
-    app.appendChild(graph);
-  });
-};
+  .then((data) => { newGraph({ ...attrs, data }) })
+  .catch((error) => {
+    console.log(`Error loading data: ${error.mesage}`)
+  })
+}
 
 const frameworks = [
   {
@@ -73,10 +85,24 @@ const frameworks = [
   }
 ];
 
-for(let framework of frameworks) {
-  newGraph({
-    route: `repos/${framework.org}/${framework.repo}/stats/participation`,
-    id: framework.repo,
-    color: framework.color
-  });
-}
+const renderGraphs = (partialRoute) => {
+  for(let framework of frameworks) {
+    let { org, repo, color } = framework;
+    fetchData({
+      route: `repos/${org}/${repo}/${partialRoute}`,
+      id: repo,
+      color: color
+    });
+  }
+};
+
+window.onload = () => {
+  const form = document.getElementById("stat-selector");
+  form.onchange = (event) => {
+    if(event.target.value === "participation") {
+      renderGraphs('stats/participation');
+    } else {
+      renderGraphs('issues/comments');
+    }
+  }
+};
